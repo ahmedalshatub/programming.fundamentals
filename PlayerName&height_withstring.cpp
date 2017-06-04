35904dd289a1161cf1a7894c782950ca0e0e906f
alshatub <ahmed.alshatub@yahoo.com>
2017-06-05 01:34:59 +0300

    Update PlayerName&height_withstring.cpp

--- PlayerName&height_withstring.cpp
+++ PlayerName&height_withstring.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
 using namespace std;
-int main() { int n=4;
-string PlayerName[n]{"david beckham","gianluigi buffon","gabriel batistuta","zinedine zidane"};
-	float x[n]={1.83,1.91,1.85,1.85};
-int j,i;
-bool flag=false;
-char ch;
-ah	:
-cout<<"enter the first letters of player name";
+int main() {
+	int n=4;
+	string PlayerName[2][n]{{"david beckham","gianluigi buffon","gabriel batistuta","zinedine zidane"}
+	,{"1.83m","1.91m","1.85m","1.85m"}};
+	int j,i;
+	bool flag=false;
+	char ch;
+	ah	:
+	cout<<"enter the first letters of player name";
 	cin>>ch;
 	for ( i = 0; i <n; ++i) {
-	if (PlayerName[i][0]==ch) {
-				for (j = i+1; j < n; ++j)
-					if (PlayerName[j][0]==ch)
-						goto ah;
-		flag=true;
-		break;}
-	}
-if (flag) {
-	cout<<"player name : "<<PlayerName[i]<<" player height : "<<x[i]<<"m";
-} else {
-	cout<<"wrong charecter";
-}
+		switch(PlayerName[0][i][0]==ch){
+			case 1:                 for (j = i+1; j < n; ++j){
+									switch(PlayerName[0][j][0]==ch){
+									case 1:goto ah; break;}}
+							flag=true;
+							break;
+		}
+		if (flag)
+			break;
+		}
+	if (flag)
+		cout<<"player name : "<<PlayerName[0][i]<<" player height : "<<PlayerName[1][i]<<"m";
+	 else
+		cout<<"wrong charecter";
 
-
-
-
-
-	return 0;
+return 0;
 }
